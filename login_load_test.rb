89f1dde2d6fc92c1b1432cde6478bb469d48e6de
Shaunak Das <shaunak.das@tokopedia.com>
2018-08-12 12:59:08 +0530

    Added homepage_load test

--- login_load_test.rb
+++ login_load_test.rb
@@ -8,9 +8,9 @@ class WrapperClient
   attr_accessor :conn,:host, :broadcast_url, :auth
 
   def initialize()
-    @host = 'http://manager.staging.connecpath.com/'
+    @host = 'https://manager.staging.connecpath.com/'
     @conn = Faraday.new(:url => @host)
-    @auth = "eyJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiMmYxNTQ5MTI4YzE2ZDg1NTMyZDEwNmMxYzkwMmU1ZWNiN2U2ZDQ0YzI0NDhkODU0YmFhNWJiNjFmOWNjMzdkZCIsInVzZXJuYW1lIjoiZ3Vlc3QiLCJob3N0IjoiaHR0cDovLzE4LjIyMS43OS40MCIsImFkbWluX3VzZXJuYW1lIjoic2hhdW5ha2RhczIwMjAiLCJhZG1pbl9hcGlfa2V5IjoiZTg1ZTg3NjAwZDcwNzIzM2M5NmQ2OTA4MGRkOTQ5YWJiYjY4OTJlN2E0YWM5ZDQ1MmY2YzU4YTI3OTlmOWY5OSIsImV4cCI6MTU2NTE1OTA5OSwiaXNzIjoiaXNzdWVyX25hbWUiLCJhdWQiOiJjbGllbnQifQ.oAW-2CKdMvDkIn-ADL2eyKFXd6kqQuw4gdfsMSfTcNM"
+    @auth = "eyJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiZWZmY2EwMzJlM2EwOWJmYjEzMzIwZTNkYTdhNjlkOGE0M2RiYWM0NGU5N2RiZjNlNzg5OGVmNDIyMjk4YWU0MyIsInVzZXJuYW1lIjoiZ3Vlc3QiLCJob3N0IjoiaHR0cDovLzE4LjE4OC4yMzguMjMyIiwiYWRtaW5fdXNlcm5hbWUiOiJzaGF1bmFrZGFzMjAyMCIsImFkbWluX2FwaV9rZXkiOiJlODVlODc2MDBkNzA3MjMzYzk2ZDY5MDgwZGQ5NDlhYmJiNjg5MmU3YTRhYzlkNDUyZjZjNThhMjc5OWY5Zjk5IiwiZXhwIjoxNTY1MTYyNTYxLCJpc3MiOiJpc3N1ZXJfbmFtZSIsImF1ZCI6ImNsaWVudCJ9._uE5LP5uZPDzEXlxo1_84PuaWjanoUXFQhNyCs6WfFc"
 
   end
 
@@ -18,20 +18,20 @@ class WrapperClient
   def post_call(location,params)
     puts "#Wrapper Service POST req:- \n#Host: #{@host} \n#Location: #{location} \n#Params: #{params.to_json} "
     response = @conn.post location, params
-    puts "#Response: " + response.body
+    puts "#Response Code: #{response.status}"
     return response
   end
 
-  # Method for POST API Call with auth
+  # Method for GET API Call with auth
   def auth_get_call(location,params,auth)
-    puts "#Wrapper Service POST req:- \n#Host: #{@host} \n#Location: #{location} \n#Params: #{params.to_json} "
+    puts "#Wrapper Service GET req:- \n#Host: #{@host} \n#Location: #{location} \n#Params: #{params.to_json} "
     response =  @conn.get do |req|
       req.url location
       req.headers['Content-Type'] = 'application/json'
       req.headers['Authorization'] = auth.to_s
       req.body = params.to_json
     end
-    puts "#Response: " + response.body
+    puts "#Response Code: #{response.status}"
     return response
   end
 
@@ -65,13 +65,21 @@ class WrapperClient
   end
 
   def homepage_test
-  	resp= JSON.parse(auth_get_call('/questions?id=5,6,7,8,9,10,11,12&limit=10&page=1', {}, @auth).body)
+  	resp= JSON.parse(auth_get_call('/questions.json?id=5,6,7,8,9,10,11,12&limit=10&page=1', {}, @auth).body)
 
     if resp.has_key?('id_stream') && 
     	resp['id_stream'].has_key?('topic_list')
-    	puts "#Test Result: Success. Question Count in response:" + resp['id_stream']['topic_list'].count
+    	puts "#Test Result: Success. Question Count in response: #{resp['id_stream']['topic_list'].count}"
     else
-    	puts "#Test Result: Failure. Response:" + resp
+    	puts "#Test Result: Failure. Response: #{resp}"
+    end
+  end
+
+  # Method for multiple homepage Calls for load testing
+  def homepage_load(count=1)
+  	for i in 0..count
+	    puts "#Trial #{i}"	
+	    homepage_test
     end
   end
 
@@ -81,7 +89,7 @@ class Test
   def initialize
     wrapper = WrapperClient.new()
     # wrapper.login_load(2)
-    wrapper.homepage_test
+    wrapper.homepage_load(2)
   end
 end
 
